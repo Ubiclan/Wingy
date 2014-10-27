2b9a208b0b81e569b74710d2f896cea6851b0acd
Elmar Abbasov <ubicray@gmail.com>
2014-10-28 00:04:18 +0400

    Initial Commit

---
+++ Assets/FlyController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyController : MonoBehaviour 
+{
+	public Animator animControl;
+	public Rigidbody2D rigid;
+	public bool flyTrigger = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		animControl = GetComponent <Animator>();
+		rigid = GetComponent <Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Debug.Log(Input.GetButtonDown("Jump"));
+		if (Input.GetButtonDown("Jump"))
+		{
+			animControl.SetTrigger("FlyTrigger");
+			rigid.AddForce(new Vector2(0,500));
+		}
+	}
+}
